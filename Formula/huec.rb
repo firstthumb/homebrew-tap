301eb0af704e2b1d212af0f06b05a6800d6996a4
firstthumb <kocamanerol@gmail.com>
2021-06-14 20:29:29 +0300

    Brew formula update for huec version v1.0.8

--- Formula/huec.rb
+++ Formula/huec.rb
@@ -5,25 +5,25 @@
 class Huec < Formula
   desc "A powerful CLI for controlling Philips Hue lights."
   homepage "https://github.com/firstthumb/huec"
-  version "1.0.7"
+  version "1.0.8"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/firstthumb/huec/releases/download/v1.0.7/huec_1.0.7_macOS_x86_64.tar.gz"
-      sha256 "d3118ab4d5c661232144745e1ff5026d93b9f0b7e2efafcb658a4194ab0804bd"
+      url "https://github.com/firstthumb/huec/releases/download/v1.0.8/huec_1.0.8_macOS_x86_64.tar.gz"
+      sha256 "8d0467ea0234795b10f6265f29bba27f76849a6df1c0b17afc62cc6c998b1da2"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/firstthumb/huec/releases/download/v1.0.7/huec_1.0.7_macOS_arm64.tar.gz"
-      sha256 "b09eebd1223692386682aaccabff37c030530a5d230102a67d71ab603c31973b"
+      url "https://github.com/firstthumb/huec/releases/download/v1.0.8/huec_1.0.8_macOS_arm64.tar.gz"
+      sha256 "09682c05dd20d1d3c18e8c30c4f2adf71c8a31fe5a3b2415ff56f3c6d6c2b0b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/firstthumb/huec/releases/download/v1.0.7/huec_1.0.7_Linux_x86_64.tar.gz"
-      sha256 "bb6e95deb9d07aee45f46748348d909e0ef48ddb1f061a9e9bfd742d271d5d8e"
+      url "https://github.com/firstthumb/huec/releases/download/v1.0.8/huec_1.0.8_Linux_x86_64.tar.gz"
+      sha256 "b040ce14d177f9a29110d420291f22a2b86be760883ceb1b9135da04f2e37cb8"
     end
   end
 
